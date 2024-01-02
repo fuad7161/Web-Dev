098d7757e4f7743de6a8aad6b14069bfafb24cd6
Fuad7161 <fuadul202@gmail.com>
2024-01-02 11:06:34 +0600

    Built and modify by myself and deploy online

---
+++ JavaScript/Projects/ToDo List with DragDrop/README.md
@@ -0,0 +1,21 @@
+<h1 align="center">To Do With Drag and Drop</h1>
+
+**This is a simple To Do List project..**
+
+
+show this project in [online](https://to-do-with-drag-drop.netlify.app/)
+
+**Front page of the project**
+![link](pic/1.png)
+
+**Write task in the task bar**
+![link](pic/2.png)
+
+**Add new task in todo list**
+![link](pic/3.png)
+
+**Give Alart when someone try to add empty name**
+![link](pic/4.png)
+
+**Drag and Drop the item**
+![link](pic/5.png)
--- JavaScript/Projects/ToDo List with DragDrop/script.js
+++ JavaScript/Projects/ToDo List with DragDrop/script.js
@@ -1,24 +1,23 @@
-/* Custom Dragula JS */
 dragula([
-    document.getElementById("to-do"),
-    document.getElementById("doing"),
-    document.getElementById("done"),
-    document.getElementById("trash")
-  ]);
-  
-  /* Vanilla JS to add a new task */
-  function addTask() {
-    /* Get task text from input */
-    var inputTask = document.getElementById("taskText").value;
-    /* Add task to the 'To Do' column */
-    document.getElementById("to-do").innerHTML +=
-      "<li class='task'><p>" + inputTask + "</p></li>";
-    /* Clear task text from input after adding task */
-    document.getElementById("taskText").value = "";
-  }
-  
-  /* Vanilla JS to delete tasks in 'Trash' column */
-  function emptyTrash() {
-    /* Clear tasks from 'Trash' column */
-    document.getElementById("trash").innerHTML = "";
-  }
+    document.getElementById('to-do'),
+    document.getElementById('doing'),
+    document.getElementById('done'),
+    document.getElementById('trash')
+]);
+
+function addTask(){
+    var inputTask = document.getElementById('taskText').value;
+    if(inputTask == ""){
+        alert('Task name is empty.')
+        return; 
+    }
+    document.getElementById('to-do').innerHTML += 
+    `<li class="task">
+    <p>${inputTask}</p>
+    </li>`
+    document.getElementById('taskText').value = ''
+}
+
+function emptyTrash(){
+    document.getElementById('trash').innerHTML = '';
+}
